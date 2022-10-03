5746a825b6c648ead2c9a6356ecef0e074430268
narek <narek@aument.company>
2022-10-03 20:41:50 +0400

    fix: getFeaturedEventWithWorkshops fixed.

--- express_sequelize/src/Modules/events/events.controller.ts
+++ express_sequelize/src/Modules/events/events.controller.ts
@@ -16,39 +16,39 @@ class EventController extends Controller {
   }
 
   public intializeRoutes() {
-        this.router.get(this.path.concat('/warmupevents'), this.getWarmupEvents.bind(this));
-        this.router.get(this.path.concat('/events'), this.getEventsWithWorkshops.bind(this));
-        this.router.get(this.path.concat('/futureevents'), this.getFutureEventWithWorkshops.bind(this));
-    }
+    this.router.get(this.path.concat('/warmupevents'), this.getWarmupEvents);
+    this.router.get(this.path.concat('/events'), this.getEventsWithWorkshops);
+    this.router.get(this.path.concat('/futureevents'), this.getFutureEventWithWorkshops);
+  }
 
-    async getWarmupEvents(req: Request, res: Response, next: NextFunction) {
-        return await this.eventsService.getWarmupEvents()
-          .then((data) => {
-              res.json(data);
-          })
-          .catch((e: Error) => {
-              next(e);
-          });
-    }
+  getWarmupEvents = async (req: Request, res: Response, next: NextFunction) => {
+    return await this.eventsService.getWarmupEvents()
+      .then((data) => {
+          res.json(data);
+      })
+      .catch((e: Error) => {
+          next(e);
+      });
+  }
 
-    async getEventsWithWorkshops(req: Request, res: Response, next: NextFunction) {
-        return await this.eventsService.getEventsWithWorkshops()
-          .then((data) => {
-              res.json(data);
-          })
-          .catch((e: Error) => {
-              next(e);
-          });
-    }
+  getEventsWithWorkshops = async (req: Request, res: Response, next: NextFunction)  => {
+    return await this.eventsService.getEventsWithWorkshops()
+      .then((data) => {
+          res.json(data);
+      })
+      .catch((e: Error) => {
+          next(e);
+      });
+  }
 
-    async getFutureEventWithWorkshops(req: Request, res: Response, next: NextFunction) {
-        return await this.eventsService.getFutureEventWithWorkshops()
-          .then((data) => {
-              res.json(data);
-          })
-          .catch((e: Error) => {
-            next(e);
-        });
+    getFutureEventWithWorkshops = async (req: Request, res: Response, next: NextFunction) => {
+      return await this.eventsService.getFutureEventWithWorkshops()
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((e: Error) => {
+          next(e);
+      });
     }
 }
 
--- express_sequelize/src/Modules/events/events.service.ts
+++ express_sequelize/src/Modules/events/events.service.ts
@@ -1,5 +1,6 @@
 import Event from './entities/event.entity';
-
+import Workshop from "./entities/workshop.entity";
+import { Op } from "sequelize";
 
 export class EventsService {
 
@@ -85,7 +86,10 @@ export class EventsService {
      */
 
   async getEventsWithWorkshops() {
-    throw new Error('TODO task 1');
+   return await Event.findAll({
+     logging: false,
+     include: { model: Workshop }
+   })
   }
 
   /* TODO: complete getFutureEventWithWorkshops so that it returns events with workshops, that have not yet started
@@ -155,6 +159,12 @@ export class EventsService {
     ```
      */
   async getFutureEventWithWorkshops() {
-    throw new Error('TODO task 2');
+    return await Event.findAll({
+      logging: false,
+      include: {
+        model: Workshop,
+        where: { start: {  [Op.gte]: Date.now() }},
+      }
+    })
   }
 }
